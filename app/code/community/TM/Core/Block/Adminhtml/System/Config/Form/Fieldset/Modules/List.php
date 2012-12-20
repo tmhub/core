b03ef9dedc94eb5aa98fdff4f7fa2a0e675430bd
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2012-12-20 17:14:48 +0200

    Compatibility with newest Magento version added

--- app/code/community/TM/Core/Block/Adminhtml/System/Config/Form/Fieldset/Modules/List.php
+++ app/code/community/TM/Core/Block/Adminhtml/System/Config/Form/Fieldset/Modules/List.php
@@ -12,14 +12,31 @@ class TM_Core_Block_Adminhtml_System_Config_Form_Fieldset_Modules_List
             if (0 !== strpos($moduleName, 'TM_')) {
                 continue;
             }
+
             if ($values->tm_link) {
-                $link = (string) $values->tm_link;
-                $moduleName = "<a href='{$link}' onclick='window.open(this.href); return false;' title='{$linkTitle}'>{$moduleName}</a>";
+                if (@is_readable(MAGENTO_ROOT . '/lib/Varien/Data/Form/Element/Link.php')) {
+                    $field = $element->addField($moduleName, 'link', array(
+                        'label'   => $moduleName,
+                        'value'   => (string) $values->version,
+                        'href'    => (string) $values->tm_link,
+                        'onclick' => 'window.open(this.href); return false;',
+                        'title'   => $linkTitle
+                    ));
+                } else {
+                    $link       = (string) $values->tm_link;
+                    $moduleName = "<a href='{$link}' onclick='window.open(this.href); return false;' title='{$linkTitle}'>{$moduleName}</a>";
+
+                    $field = $element->addField($moduleName, 'label', array(
+                        'label' => $moduleName,
+                        'value' => (string) $values->version
+                    ));
+                }
+            } else {
+                $field = $element->addField($moduleName, 'label', array(
+                    'label' => $moduleName,
+                    'value' => (string) $values->version
+                ));
             }
-            $field = $element->addField($moduleName, 'label', array(
-                'label' => $moduleName,
-                'value' => (string) $values->version
-            ));
             $html .= $field->toHtml();
         }
         $html .= $this->_getFooterHtml($element);
