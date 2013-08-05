2dfded9274ad106fde2dedc7498765448a8132f1
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2013-08-05 10:19:35 +0300

    Old magento versions compatibility

--- app/code/community/TM/Core/sql/tm_core_setup/mysql4-install-1.0.0.php
+++ app/code/community/TM/Core/sql/tm_core_setup/mysql4-install-1.0.0.php
@@ -8,16 +8,19 @@ $installer->startSetup();
 /**
  * Create table 'tmcore/module'
  */
+$typeText = defined('Varien_Db_Ddl_Table::TYPE_TEXT')
+    ? Varien_Db_Ddl_Table::TYPE_TEXT : Varien_Db_Ddl_Table::TYPE_VARCHAR;
+
 $table = $installer->getConnection()
     ->newTable($installer->getTable('tmcore/module'))
-    ->addColumn('code', Varien_Db_Ddl_Table::TYPE_TEXT, 50, array(
+    ->addColumn('code', $typeText, 50, array(
             'nullable'  => false,
             'primary'   => true,
         )
     )
 //    ->addColumn('version', Varien_Db_Ddl_Table::TYPE_TEXT, 50)
-    ->addColumn('data_version', Varien_Db_Ddl_Table::TYPE_TEXT, 50)
-    ->addColumn('license_key', Varien_Db_Ddl_Table::TYPE_TEXT, 32);
+    ->addColumn('data_version', $typeText, 50)
+    ->addColumn('license_key', $typeText, 32);
 $installer->getConnection()->createTable($table);
 
 $installer->endSetup();
