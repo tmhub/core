3ae0928147bee5025a1118460566d46fc53c9730
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2015-01-14 17:50:45 +0200

    Added method to compare current design package

--- app/code/community/TM/Core/Helper/Data.php
+++ app/code/community/TM/Core/Helper/Data.php
@@ -2,5 +2,9 @@
 
 class TM_Core_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+    public function isDesignPackageEquals($packageName)
+    {
+        $package = Mage::getSingleton('core/design_package');
+        return $package->getPackageName() === $packageName;
+    }
 }
