b6aef7ed0be13cb0c3dff2ad20fc4dd7f1f37d21
Vitaliy <vitaliy@mage2cloud.com>
2017-07-25 14:05:51 +0300

    fix for layout updates in widgets interface in magento admin

--- app/code/community/TM/Core/Helper/Data.php
+++ app/code/community/TM/Core/Helper/Data.php
@@ -4,6 +4,11 @@ class TM_Core_Helper_Data extends Mage_Core_Helper_Abstract
 {
     public function isDesignPackageEquals($packageName)
     {
+        if ($this->_getRequest()->getControllerName() == 'widget_instance'
+            && $this->_getRequest()->getActionName() == 'blocks') {
+            //fix for layout updates in widgets interface in magento admin
+            return true;
+        };
         $package = Mage::getSingleton('core/design_package');
         return $package->getPackageName() === $packageName;
     }
