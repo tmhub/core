2640f6369252a7e0b78b0e73bd2a24075a959a78
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2017-08-25 12:15:24 +0300

    RequireOnce method added. Usefull for third-party modules integration

--- app/code/community/TM/Core/Helper/Data.php
+++ app/code/community/TM/Core/Helper/Data.php
@@ -12,4 +12,17 @@ class TM_Core_Helper_Data extends Mage_Core_Helper_Abstract
         $package = Mage::getSingleton('core/design_package');
         return $package->getPackageName() === $packageName;
     }
+
+    /**
+     * Generates require_once statement for supplied path
+     * @param  string $path
+     * @return void
+     */
+    public function requireOnce($path)
+    {
+        if (defined('COMPILER_INCLUDE_PATH')) {
+            $path = str_replace('/', '_', $path);
+        }
+        require_once $path;
+    }
 }
