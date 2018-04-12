864574df5c1a0d7bd0af3bc740fc468ba883c332
Makis Palasis <37408939+mpalasis@users.noreply.github.com>
2018-04-12 19:54:27 +0300

    Expected semicolon ; after "}"
    NetBeans complains: 
    `Expected semicolon ; after "}" `
    on var declarations that don't close with ;
    
    js formatting and folding breaks past that point.

--- js/tm/lib/event.simulate.js
+++ js/tm/lib/event.simulate.js
@@ -13,7 +13,7 @@
   var eventMatchers = {
     'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
     'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
-  }
+  };
   var defaultOptions = {
     pointerX: 0,
     pointerY: 0,
@@ -24,7 +24,7 @@
     metaKey: false,
     bubbles: true,
     cancelable: true
-  }
+  };
   
   Event.simulate = function(element, eventName) {
     var options = Object.extend(Object.clone(defaultOptions), arguments[2] || { });
